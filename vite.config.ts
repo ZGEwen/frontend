423eb9361f574f944872c20e023552b6be2eed2c
zgw <2545717263@qq.com>
2021-08-29 17:55:34 +0800

    解决路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /**
+   * 在生产中的基本公共路径
+   * @default '/'
+   */
+  base: './',
   plugins: [vue()]
 })
